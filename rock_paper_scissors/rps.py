10ba1fe10bbecc636e9cedf9978eee8bf1688f4c
DimejiAre <olardi2@gmail.com>
2020-02-20 18:34:19 +0100

    add solution for rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,36 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+# def rock_paper_scissors(n):
+#   outcomes = []
+#   plays = ['rock', 'paper', 'scissors']
 
+#   def find_results(rounds_to_go, results=[]):
+#     # if rounds = 0
+#       # append results to outcomes
+#       # then retun
+#     # iterate over plays
+#       # new result concatenate our results with our plays ** newres = results + [play]
+#       # recursive call using roundstogo - 1, newresult
+
+#   # call find_results pass in n, []
+#       pass 
+#   # return the outcomes
+#   pass
+
+def rock_paper_scissors(n):
+  outcomes = []
+  plays = ['rock', 'paper', 'scissors']
+
+  def find_results(rounds_to_go, results=[]):
+    if rounds_to_go == 0:
+      return outcomes.append(results)
+    for play in plays:
+      new_result = results + [play]
+      find_results(rounds_to_go - 1, new_result)
+
+  find_results(n, [])
+  return outcomes
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
